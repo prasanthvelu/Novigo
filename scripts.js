a970ec74a0d8af0cb585d0d6a447e724a1a1478e
prasanthvelu <143422340+prasanthvelu@users.noreply.github.com>
2023-09-28 21:20:48 +0530

    Discovery call button mobile view

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 "use strict";
-const menulistDiscoverycall = document.querySelector(".menulist_discoverycall")
+const menulistDiscoverycall = document.querySelectorAll(".menulist_discoverycall")
 const hamburger = document.querySelector(".hamburger")
 hamburger.addEventListener("click", () => {
-    menulistDiscoverycall.classList.toggle("menulist_discoverycall")
+    menulistDiscoverycall.forEach((element) => {
+        element.classList.toggle("menulist_discoverycall")
+    })
 })
