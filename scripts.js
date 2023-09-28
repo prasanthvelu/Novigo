cdf7d8ada746e90d17287c219ec615cc7df62d6d
prasanthvelu <143422340+prasanthvelu@users.noreply.github.com>
2023-09-28 20:40:48 +0530

    footer

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 "use strict";
-const headerUL = document.querySelector("header ul")
-const headerBtn = document.querySelector(".book_discovery_call_2")
+const menulistDiscoverycall = document.querySelector(".menulist_discoverycall")
 const hamburger = document.querySelector(".hamburger")
 hamburger.addEventListener("click", ()=>{
-    headerUL.classList.toggle("menulist_discoverycall")
-    headerBtn.classList.toggle("menulist_discoverycall")
+    menulistDiscoverycall.classList.toggle("menulist_discoverycall")
 })
